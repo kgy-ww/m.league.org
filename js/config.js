0b2e60729a306f0c415d9520b94bc0c1d1ce65db
kgy-ww <75308686+kgy-ww@users.noreply.github.com>
2026-06-02 01:19:19 +0900

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,3 +1,4 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbyaLrQdHY3TzYa23RBApxJ-Z4UCjsumhgU9InGaoFQqbF8sKWChOVrbzFzGDoj_Gd8/exec"
 const CONFIG = {
     // 開発時は適切な値を入れる
     API_KEY: ""
